e57913d36c05e26d854cb2fc2cf37cc1e06c5033
NyasaLuharuka <73618884+NyasaLuharuka@users.noreply.github.com>
2021-06-22 14:31:41 -0500

    Update website.js

--- website.js
+++ website.js
@@ -1,1 +1,20 @@
+var AIRFOOD = document.getElementById("airfood");
+
+airfood.onclick = function() {
+    alert("Number 1 Best Selling Food in TOWN")
+}
+
+
+var btn = document.getElementById("notmemes");
+
+btn.onclick = function() {
+    console.log("YOU ARE PAYING FOR AIRFOOD")
+}
+
+
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "20px Segoe Print";
+ctx.fillText("Hello World, Click RIGHT", 5, 50);
 
